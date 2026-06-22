5a66cc2e81e0cac75fa50cf2b6a6a28ad37de88a
joaopedro <jpssantosos5874@gmail.com>
2026-06-22 01:11:32 -0300

    refact: remove old order package replaced by shipment

--- backend/src/main/java/com/jps/jps/order/OrderRepository.java
+++
@@ -1,5 +0,0 @@
-package com.jps.jps.order;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface OrderRepository extends CassandraRepository<Order, String> {}
