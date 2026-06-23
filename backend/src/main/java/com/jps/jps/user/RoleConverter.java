0146e87612fc41f6666670ec026ba01d5c80facd
joaopedro <jpssantosos5874@gmail.com>
2026-06-22 23:14:21 -0300

    fix: make RoleConverter use concrete classes and Spring Converter

--- backend/src/main/java/com/jps/jps/user/RoleConverter.java
+++ backend/src/main/java/com/jps/jps/user/RoleConverter.java
@@ -1,20 +1,22 @@
 package com.jps.jps.user;
 
-import com.fasterxml.jackson.databind.util.Converter;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
 
 public class RoleConverter {
 
     @ReadingConverter
-    public static abstract class Read implements Converter<Integer, Role> {
+    public static class Read implements Converter<Integer, Role> {
+        @Override
         public Role convert(Integer id) {
             return Role.fromId(id);
         }
     }
 
     @WritingConverter
-    public static abstract class Write implements Converter<Role, Integer> {
+    public static class Write implements Converter<Role, Integer> {
+        @Override
         public Integer convert(Role role) {
             return role.getId();
         }
